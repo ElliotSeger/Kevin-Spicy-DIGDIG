db84768e64104151aa719958c3f090857190c1ed
Noah-Lundqvist <35488281+Noah-Lundqvist@users.noreply.github.com>
2018-05-04 12:42:05 +0200

    Crosshair
    Crosshair

--- Kevin spicy GAME/Kevin spicy GAME/Player.cs
+++ Kevin spicy GAME/Kevin spicy GAME/Player.cs
@@ -8,16 +8,18 @@ namespace Kevin_spicy_GAME
     {
         Texture2D kevinSpaceshipTexture;
         Rectangle spaceshipRectangle;
-        // lol
         Vector2 position;
+        Vector2 crosshairpos;
         Vector2 scale;
         Vector2 offset;
+        Vector2 mousepos;
         Color spaceshipColor;
         float speed;
         float attackInterval;
         float attackSpeed;
         float rotation;
         Texture2D bulletGreenTexture;
+        Texture2D crosshairTexture;
         private Texture2D texture2D;
         private Vector2 vector21;
         private int v1;
@@ -28,7 +30,9 @@ namespace Kevin_spicy_GAME
         public Player()
         {
             kevinSpaceshipTexture = Game1.LoadedTextures["Ship"];
+
             position = new Vector2(0, 450);
+            crosshairpos = new Vector2(1750, 430);
             speed = 300;
             attackInterval = 0.2f;
             rotation = 0;
@@ -38,6 +42,7 @@ namespace Kevin_spicy_GAME
             offset = (kevinSpaceshipTexture.Bounds.Size.ToVector2() * 0.5f) * scale;
             spaceshipRectangle = new Rectangle((position - offset).ToPoint(), (kevinSpaceshipTexture.Bounds.Size.ToVector2() * scale).ToPoint());
             bulletGreenTexture = Game1.LoadedTextures["Bullet"];
+            crosshairTexture = Game1.LoadedTextures["Crosshair"];
         }
 
         public Player(Texture2D texture2D, Vector2 vector21, int v1, Vector2 vector22, int v2, Color white)
@@ -76,6 +81,15 @@ namespace Kevin_spicy_GAME
             {
                 position += (-Vector2.UnitY * speed * deltaTime * 2);
             }
+            //crosshair movement
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                crosshairpos += (Vector2.UnitY * speed * deltaTime * 2);
+            }
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                crosshairpos += (-Vector2.UnitY * speed * deltaTime * 2);
+            }
 
             if (position.X <= 40)
             {
@@ -85,9 +99,9 @@ namespace Kevin_spicy_GAME
             {
                 position.X = (window.ClientBounds.Width - kevinSpaceshipTexture.Width * scale.X);
             }
-            if (position.Y <= 10)
+            if (position.Y <= -35)
             {
-                position.Y = 10;
+                position.Y = -35;
             }
             if (position.Y >= (window.ClientBounds.Height - kevinSpaceshipTexture.Height * scale.Y))
             {
@@ -106,7 +120,10 @@ namespace Kevin_spicy_GAME
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(kevinSpaceshipTexture, position, null, spaceshipColor, rotation, offset, scale, SpriteEffects.None, 0);
+
+            spriteBatch.Draw(crosshairTexture, crosshairpos, null, spaceshipColor, rotation, offset, scale, SpriteEffects.None, 0);
         }
+       
 
         public void Shoot()
         {
